805d9dcb1b274ade6cce9f649066d9c702a586b4
Oussama Boumaad <magixusneo@live.fr>
2018-11-20 23:06:56 +0100

    this is first commit

---
+++ docsearch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DocsearchConfig(AppConfig):
+    name = 'docsearch'
---
+++ docsearch/static/assets/js/docsearch.js
@@ -0,0 +1,13 @@
+
+
+/*  we assume that jquery is installed */
+$(document).ready(function() {
+
+    /*console.log(document.location.pathname);*/
+    //récupération du lien
+    var place = '#'+document.location.pathname.split('/')[1];
+    //$( '.navbar-nav' ).find( 'li.active' ).removeClass('active');
+    $( place ).addClass( 'active' );
+});
+
+{% if data %}
