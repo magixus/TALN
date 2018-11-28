5ae08baa9213f2c7e5206949075a21faa4357c8a
magixus <magixusneo@live.fr>
2018-11-29 00:00:39 +0100

    change ALLOWED_HOSTS config

--- TALN/settings.py
+++ TALN/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'vi!hk2z@c*)i8tv#kyh@xxzg+#2a($9_#mcm)o258*^hn0-#sh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.herokuapp.com'
+]
 
 
 # Application definition
